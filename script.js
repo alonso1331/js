3f0785bfd4af3900cf492f2aa9c5651c327f788f
Victoria <odin0330@gmail.com>
2022-05-23 12:48:46 +0800

    我的姓名身高體重

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
-document.write("test");
+document.write("About Me" + "</br>");
 alert("alert"); //彈跳出警告視窗，只能按確認
 console.log("123"); //不會顯示在網頁上，要在F12看
 prompt("請輸入年齡","25"); //彈跳出輸入文字的對話視窗
 confirm("請確認");//彈跳出需要使用者確認的對話視窗，可以選擇確認與取消
+
+let myName = "Cing", myAge = 18, myHeight = 165, myWeight = 50;
+
+document.write("我是" + myName + "</br>");
+document.write("身高" + myHeight + "公分，體重" + myWeight +"公斤");
+document.write("今年" + myAge);

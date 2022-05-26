07c9c5088d3d11bf9b1e24dd178efbbde1821ebc
Victoria <odin0330@gmail.com>
2022-05-26 09:21:29 +0800

    1.有指定與未指定變數console.log的差別 2.typeof找屬性的運用

--- script0526.js
+++ script0526.js
@@ -6,3 +6,16 @@ console.log(num1 === num2);
 // = 是指派
 // == 等同於數學運算的=
 // === 除了數值以外，連屬性(比如數字或字串)的性質都要相同
+
+let x ;
+let y = "";
+let z = null;
+console.log(x);
+console.log(y);
+console.log(z);
+
+let a = true;
+let b = "true";
+console.log(typeof a);
+console.log(typeof a, a);
+console.log(typeof(b), b);

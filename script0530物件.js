147dd5517aabd83884bcd58c23e092a10ecc73de
Victoria <odin0330@gmail.com>
2022-05-31 07:44:30 +0800

    Arrow Functions 箭頭函數

--- script0530物件.js
+++ script0530物件.js
@@ -15,17 +15,51 @@
 // document.write(person2.sayHello() + "</br>" + person3.sayHello())
 
 //4.5.JavaScript－認識DOM(Document Object Model)
+//一、認識 DOM（Document Object Model）
 // function sayHi(){
 //     console.log("Hi");
 // }
 
+//二、Window Object
 // let myInterval = window.setInterval(sayHi, 3000); //每3000毫秒=3秒會跳一次Hi
 
 // function stop() {
 //     clearInterval(myInterval);
 // }
 
-let myH1 = document.getElementById("first");
-let myP = document.getElementsByClassName("Second");
-console.log(myH1);
-console.log(myP);
+//三、尋找元素節點
+// let myH1 = document.getElementById("first");
+// let myP = document.getElementsByClassName("second");
+// console.log(myH1);
+// console.log(myP);
+
+// let first = window.document.querySelector("#first");
+// console.log(first);
+// let second = window.document.querySelectorAll("p.second");
+// console.log(second);
+
+//四、Arrow Functions 箭頭函數
+function sayHi(){
+    console.log("Hi");
+}
+
+let sayHi = () =>{
+    console.log("Hi");
+}
+
+sayHi();
+//function和=>箭頭函數兩個語法效果是一樣的
+//差別在於sayHi()如果放在function前也能執行，但是放在=>箭頭函數前，就不能執行，會顯示未定義
+
+let TCNR = {
+    name: "勞動部勞動力發展署中彰投分署",
+    myName(){
+        console.log("TCNR是" + this.name) + ":";
+    },
+    address: () => {
+        console.log("TCNR的地址是台中市西屯區工業區一路100號");
+    }
+}
+
+TCNR.myName();
+TCNR.address();
